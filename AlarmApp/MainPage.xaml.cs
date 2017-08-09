258dabaa27deae6c812b7cd07ad98515853cb716
Brad Tapolcsanyi <bradtapo@gmail.com>
2017-08-08 20:45:01 -0400

    Added files and created Repo

---
+++ AlarmApp/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AlarmApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btn_Snooze_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Dismiss_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_Source_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_CloseNPR_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtBox_Time_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
